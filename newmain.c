f19f352fa23ab3832fa1c8d6e7c1f70f0540d34c
Gon2153 <125921214+Gon2153@users.noreply.github.com>
2023-03-24 00:13:37 -0600

    analogicos

--- newmain.c
+++ newmain.c
@@ -31,22 +31,72 @@
 ////////////prototipos
 void setup (void);
 
-void __interrupt() isr (void){
-/////////interrupciones
-    ////interrupcion del ADC
-if (PIR1bits.ADIF){
-    PIR1bits.ADIF = 0; /// chequeamos la bandera de interrupcion del ADC
+/////////////////interupciones 
+
+void __interrupt() ISR() {
+    if (PIR1bits.ADIF) { // Interrupción del ADC
+        PORTD = ADRESH; // Muestra los 8 bits superiores en el puerto D
+        PIR1bits.ADIF = 0; // Limpia la bandera de interrupción del ADC
     }
 
-////////////////interrupcion del tmr0
-    if (INTCONbits.T0IF) { // Si se produjo una interrupci?n por desbordamiento de TMR0
-        INTCONbits.T0IF = 0; // Limpia la bandera de interrupci?n
-        TMR0 = 6;           // Reinicia el valor de TMR0
+    if (INTCONbits.T0IF) { // Interrupción del TMR0
+        // Aquí puedes agregar código para ejecutar en cada interrupción del TMR0
+        INTCONbits.T0IF = 0; // Limpia la bandera de interrupción del TMR0
+        TMR0 = 216; ///VALOR INICIAL DEL TMR0
     }
 }
 
 ////////////////////principal
-void main(void){
-    setup{};
+void main(void) {
+    setup();
+
+    while(1) {
+        __delay_ms(50); // Espera 50ms
+        ADCON0bits.GO = 1; // Inicia la conversión ADC
+    }
+}
+
+/////////////CONFIGURACION 
+void setup (void){
+    ANSEL = 0B00000011; //CONFIGURAR A AN0 Y AN1 COMO ENTRADAS ANALOGIVCAS
+    ANSELH = 0; 
+    
+    TRISD = 0 ; ///PORTD COMO SALIDA
+    PORTD = 0; //SE INICIA EL PORTD
+    
+    TRISA = 0B00000011;  //RA0 Y RA1 COMO ENTRADAS
+    PORTA = 0; //INICIA PORTA
+    
+        //////////////oscilador
+    OSCCONbits.IRCF = 0b111 ; ///8Mhz
+    OSCCONbits.SCS = 1;
+    
+
+    /////////////// tmr0
+    OPTION_REGbits.T0CS = 0; //Usar Timer0 con Fosc/4
+    OPTION_REGbits.PSA = 0; //Prescaler con el Timer0
+    OPTION_REGbits.PS2 = 1; //Prescaler de 256
+    OPTION_REGbits.PS1 = 1;
+    OPTION_REGbits.PS0 = 1;  
+    TMR0 = 216; ///VALOR INICIAL DEL TMR0
+    
+    /////////Banderas e interrupciones
+    INTCONbits.T0IF = 0; //interrupcion del tmr0
+    INTCONbits.T0IE = 1;///interrupcion del tmr0
+    INTCONbits.PEIE = 1;/// INTERRUPCIONES DE PUERTO
+    PIE1bits.ADIE = 1; ///interrupcion del adc
+    PIR1bits.ADIF = 0; //FLAG DEL ADC EN 0
+    INTCONbits.GIE = 1; //globales
+    
+    
+    ///////CONFIGURACIN DEL ADC
+    ADCON1bits.ADFM = 0; ///justificado a la izquuierda
+    ADCON1bits.VCFG0 = 0; //vdd como referenias
+    ADCON1bits.VCFG1 = 0; //vss como referencias
+    
+    ADCON0bits.ADCS = 0b10 ; ///fosc/32 
+    ADCON0bits.CHS = 0b0000; ///chs 0000 an0 selecciona canal
+    
+    ADCON0bits.ADON = 1; //inicial el adc
     
 }
